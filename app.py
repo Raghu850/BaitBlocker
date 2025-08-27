594df937857cdc1638e7676c956114c6ac3e75ac
unknown <raghuram829729@gmail.com>
2025-08-28 00:13:36 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1312,4 +1312,5 @@ def change_password():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=False)
